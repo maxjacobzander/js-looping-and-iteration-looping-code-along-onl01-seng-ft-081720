1b55a626da43745de99a908eecbd752deec19a52
maxjacobzander <maxjacobzander@gmail.com>
2020-11-24 12:16:56 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, occasion) {
+    let thankYouCardArray = [];
+    for(let i = 0; i < name.length; i++) {
+        thankYouCardArray.push(`Thank you, ${name[i]}, for the wonderful ${occasion} gift!`);
+    };
+    return thankYouCardArray;
+}
+
+function countDown(i) {
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
